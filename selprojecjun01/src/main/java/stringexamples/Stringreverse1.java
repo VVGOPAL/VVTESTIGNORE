b735bc83f468ed17cebc88a08aa9127d9d0fc2b7
GOPAL <nagen@DESKTOP-Q15TQC0>
2018-06-12 11:01:06 +0530

    firstcommit

---
+++ selprojecjun01/src/main/java/stringexamples/Stringreverse1.java
@@ -0,0 +1,28 @@
+package stringexamples;
+
+public class Stringreverse1 
+{
+
+	public static void main(String[] args)
+	 
+	 {
+		 //String revertedstring= Stringreverse.reverseeachword("Omnience Events India Pvt Ltd");
+		 
+		 System.out.println(reverseWord("omnience events india pvt ltd"));    
+	 }
+	
+	private static String reverseWord(String str)
+	{  
+	    String words[]=str.split("\\s");  
+	    String reverseWord="";  
+	    for(String w:words)
+	    {  
+	        StringBuilder sb=new StringBuilder(w);  
+	        sb.reverse();  
+	        reverseWord+=sb.toString()+" ";  
+	    }  
+	    return reverseWord.trim();  
+	}  
+	
+
+}
